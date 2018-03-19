abaeed640d05604bbc75103bf47a65c52ee7517a
Nicolás Peralta <nico@mejuri.com>
2018-03-19 20:57:32 -0300

    initial commit

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default App;
